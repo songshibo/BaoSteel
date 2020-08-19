1b249a055fb11d856067ba279801dabfff216983
SaberStereo <stereo.bingo@gmail.com>
2020-08-19 17:05:28 +0800

    add layer manager & remove external config reader & move config reader to Util

---
+++ Assets/Scripts/Managers/LayerManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerManager : Singleton<LayerManager>
+{
+    public void SetBackgroundColorMaskWeight(float lerpValue)
+    {
+        Resources.Load<Material>("ColorMaskBlitMat").SetFloat("_LerpValue", lerpValue);
+    }
+
+    public void AddToHighlight(GameObject gameObject)
+    {
+        gameObject.layer = LayerMask.NameToLayer("highlight");
+        SetBackgroundColorMaskWeight(0.8f);
+    }
+
+    public void MoveFromHighlight(GameObject gameObject)
+    {
+        gameObject.layer = LayerMask.NameToLayer("default");
+    }
+
+    public void AddAllToHighlight(GameObject[] gameObjects)
+    {
+        for (int i = 0; i < gameObjects.Length; i++)
+            AddToHighlight(gameObjects[i]);
+    }
+
+    public void MoveAllFromHighlight(GameObject[] gameObjects)
+    {
+        for (int i = 0; i < gameObjects.Length; i++)
+            MoveFromHighlight(gameObjects[i]);
+
+        if (!Util.IsAnyObjectsInLayer("highlight"))
+            SetBackgroundColorMaskWeight(0.0f);
+    }
+}
--- Assets/Scripts/Static/Util.cs
+++ Assets/Scripts/Static/Util.cs
@@ -1,10 +1,27 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
+using System.IO;
 
 public static class Util
 {
+    public static string ReadConfigFile(string filename)
+    {
+        string path = Application.dataPath + "/Config/" + filename;
+        try
+        {
+            using (FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                StreamReader streamReader = new StreamReader(fs, true);
+                return streamReader.ReadToEnd();
+            }
+        }
+        catch (FileNotFoundException ex)
+        {
+            Debug.LogError(ex);
+            return null;
+        }
+    }
     /// <summary>
     /// find all distinct gameobject array by tags
     /// </summary>
@@ -23,5 +40,30 @@ public static class Util
     public static float WrapAngle(float angle)
     {
         return angle > 180 ? angle - 360 : angle;
+    }
+
+    public static GameObject[] FindObjectsInLayer(string layerName)
+    {
+        int layer = LayerMask.NameToLayer(layerName);
+        GameObject[] objects = Object.FindObjectsOfType<GameObject>();
+        List<GameObject> objectsInLayer = new List<GameObject>();
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i].layer == layer)
+                objectsInLayer.Add(objects[i]);
+        }
+        return objects.ToArray();
+    }
+
+    public static bool IsAnyObjectsInLayer(string layerName)
+    {
+        int layer = LayerMask.NameToLayer(layerName);
+        GameObject[] objects = Object.FindObjectsOfType<GameObject>();
+        for (int i = 0; i < objects.Length; i++)
+        {
+            if (objects[i].layer == layer)
+                return true;
+        }
+        return false;
     }
 }
