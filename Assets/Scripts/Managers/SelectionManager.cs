737bac9d2f58e2782a9e82f54ce94aa3255bea78
SaberStereo <stereo.bingo@gmail.com>
2020-08-28 00:31:23 +0800

    add selection manager

---
+++ Assets/Scripts/Managers/SelectionManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityFx.Outline;
+
+public class SelectionManager : Singleton<SelectionManager>
+{
+    private OutlineBuilder GetOutlineBuilder()
+    {
+        OutlineBuilder outlineBuilder = null;
+        try
+        {
+            outlineBuilder = UnityEngine.Object.FindObjectOfType<OutlineBuilder>();
+        }
+        catch (NullReferenceException e)
+        {
+            Debug.Log("No OutlineBuilder:" + e);
+        }
+        return outlineBuilder;
+    }
+
+    public void AddToOutlineList(GameObject gameObject)
+    {
+        OutlineBuilder outline = GetOutlineBuilder();
+        outline.OutlineLayers.GetOrAddLayer(0).Add(gameObject);
+    }
+
+    public void MoveFromOutlineList(GameObject gameObject)
+    {
+        OutlineBuilder outline = GetOutlineBuilder();
+        outline.OutlineLayers.GetOrAddLayer(0).Remove(gameObject);
+    }
+}
---
+++ Assets/Scripts/TEST/outlineTest.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class outlineTest : MonoBehaviour
+{
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SelectionManager.Instance.MoveFromOutlineList(GameObject.Find("chute"));
+        }
+    }
+}
