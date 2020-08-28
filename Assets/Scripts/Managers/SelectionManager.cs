45d93c9ea0a126ab5e19a0b723cb26bcabfff275
SaberStereo <stereo.bingo@gmail.com>
2020-08-28 14:25:38 +0800

    add public function for selection manager

--- Assets/Scripts/Managers/SelectionManager.cs
+++ Assets/Scripts/Managers/SelectionManager.cs
@@ -20,15 +20,61 @@ public class SelectionManager : Singleton<SelectionManager>
         return outlineBuilder;
     }
 
-    public void AddToOutlineList(GameObject gameObject)
+    /// <summary>
+    /// 给单个物体添加outline效果
+    /// </summary>
+    /// <param name="gameObjects"></param>
+    /// <param name="layerIndex">OutlineLayerCollection的对应outline配置</param>
+    public void AddToOutlineList(GameObject gameObject, int layerIndex = 0)
     {
         OutlineBuilder outline = GetOutlineBuilder();
-        outline.OutlineLayers.GetOrAddLayer(0).Add(gameObject);
+        outline.OutlineLayers.GetOrAddLayer(layerIndex).Add(gameObject);
     }
 
-    public void MoveFromOutlineList(GameObject gameObject)
+    /// <summary>
+    /// 移除单个物体的outline效果
+    /// </summary>
+    /// <param name="gameObjects"></param>
+    /// <param name="layerIndex">OutlineLayerCollection的对应outline配置</param>
+    public void MoveFromOutlineList(GameObject gameObject, int layerIndex = 0)
     {
         OutlineBuilder outline = GetOutlineBuilder();
-        outline.OutlineLayers.GetOrAddLayer(0).Remove(gameObject);
+        outline.OutlineLayers.GetOrAddLayer(layerIndex).Remove(gameObject);
+    }
+
+    /// <summary>
+    /// 给多个物体添加outline效果
+    /// </summary>
+    /// <param name="gameObjects">物体的数组</param>
+    /// <param name="layerIndex">OutlineLayerCollection的对应outline配置</param>
+    public void AddAllToOutlineList(GameObject[] gameObjects, int layerIndex = 0)
+    {
+        OutlineBuilder outline = GetOutlineBuilder();
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            outline.OutlineLayers.GetOrAddLayer(layerIndex).Add(gameObjects[i]);
+        }
+    }
+
+    /// <summary>
+    /// 移除多个物体的outline效果
+    /// </summary>
+    /// <param name="gameObjects">物体的数组</param>
+    /// <param name="layerIndex">OutlineLayerCollection的对应outline配置</param>
+    public void MoveAllFromOutlineList(GameObject[] gameObjects, int layerIndex = 0)
+    {
+        OutlineBuilder outline = GetOutlineBuilder();
+        for (int i = 0; i < gameObjects.Length; i++)
+        {
+            outline.OutlineLayers.GetOrAddLayer(layerIndex).Remove(gameObjects[i]);
+        }
+    }
+
+    /// <summary>
+    /// 清除所有层的gameobject
+    /// </summary>
+    public void ClearLayersContent()
+    {
+        GetOutlineBuilder().OutlineLayers.ClearLayerContent();
     }
 }
