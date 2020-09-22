5df52b9f1b7f3495b92d9f99605b545ac422ad21
fashionguy <3301225509@qq.com>
2020-09-22 18:01:43 +0800

    change thermocouple detail position by left and right

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -11,6 +11,8 @@ public class UIManager : MonoSingleton<UIManager>
 {
     public CustomDropdown clipDropDown;
     public DropdownMultiSelect layerDropDown;
+    private GameObject EnterExitInfo;
+    private Vector2 ThermocouplePanel_Width_Height;
 
     public void InitializeUI(string[] configClip, string[] configShowPart)
     {
@@ -67,7 +69,8 @@ public class UIManager : MonoSingleton<UIManager>
             //obj.GetComponent<EnterExitOutline>().SetTargets(config[1]);
         }
 
-
+        EnterExitInfo = GameObject.Find("EnterExitInfo");
+        EnterExitInfo.SetActive(false);
         Invoke("GenerateThermoUI", 5);
     }
 
@@ -125,6 +128,8 @@ public class UIManager : MonoSingleton<UIManager>
     {
         GameObject prefab = (GameObject)Resources.Load("Prefabs/button");
         GameObject root = GameObject.Find("TemperaturePanel");
+        ThermocouplePanel_Width_Height = root.GetComponent<RectTransform>().sizeDelta;
+
         float xRatio = root.GetComponent<RectTransform>().sizeDelta.x / 360;
         float yRatio = root.GetComponent<RectTransform>().sizeDelta.y / 55;
         print(root.GetComponent<RectTransform>().sizeDelta.x);
@@ -145,7 +150,7 @@ public class UIManager : MonoSingleton<UIManager>
             UIobj.name = thermo.name;
             UIobj.transform.Find("height").GetComponent<Text>().text = position.y.ToString("0.###") + "m";
             UIobj.transform.Find("angle").GetComponent<Text>().text = angle.ToString("0") + "°";
-
+            UIobj.transform.Find("temperature").GetComponent<Text>().text = "0°C";
             UIobj.GetComponent<Button>().onClick.AddListener(delegate () { OnClick(thermo, UIobj); });
 
             EventTrigger eventTrigger = UIobj.AddComponent<EventTrigger>();
@@ -178,11 +183,24 @@ public class UIManager : MonoSingleton<UIManager>
     {
         if (flag)
         {
-            print("show");
+            EnterExitInfo.SetActive(true);
+            string content = "编号：" + "TE4650" + "\n温度：" + obj.transform.Find("temperature").GetComponent<Text>().text +
+                "\n高度：" + obj.transform.Find("height").GetComponent<Text>().text + "\n角度：" + obj.transform.Find("angle").GetComponent<Text>().text;
+            EnterExitInfo.GetComponent<Text>().text = content;
+            float x = obj.GetComponent<RectTransform>().localPosition.x;
+            float y = obj.GetComponent<RectTransform>().localPosition.y;
+
+            if (x < ThermocouplePanel_Width_Height.x / 2)
+            {
+                // 左边的热电偶信息 要显示在 该热电偶的右边，因为显示在左边的话，边界热电偶信息显示就有问题。
+                x += EnterExitInfo.GetComponent<RectTransform>().sizeDelta.x + 20;
+
+            }
+            EnterExitInfo.GetComponent<RectTransform>().localPosition = new Vector2(x, y);
         }
         else
         {
-            print("hide");
+            EnterExitInfo.SetActive(false);
         }
     }
 }
