a882cd89b8a7cc076d3d03fc29dfae45ab496d56
fashionguy <3301225509@qq.com>
2020-09-29 20:04:26 +0800

    use coroutine to control batch layer

--- Assets/Scripts/Managers/BatchManager.cs
+++ Assets/Scripts/Managers/BatchManager.cs
@@ -4,63 +4,100 @@ using UnityEngine;
 using Newtonsoft.Json.Linq;
 using UnityEngine.UI;
 using Newtonsoft.Json;
+using System;
 
 public class BatchManager : MonoSingleton<BatchManager>
 {
-    private Dictionary<string, float> data;
-    private int start;
+    private Vector3 from;
+    private Vector3 target;
+    private float moveSpeed;
+    private int count = 1;
+
     private void Start()
     {
-        data = new Dictionary<string, float>();
-        for (int i = 0; i < 17; i++)
-        {
-            data.Add("layer" + i.ToString(), 23 + i);
-        }
-        start = 0;
-        
+        from = new Vector3(2.5f, 41, 0);
+        target = new Vector3(2.5f, 21, 0);
+        moveSpeed = 1f;
+
     }
 
-    private void Update()
+    IEnumerator GenerateLayer(string number)
     {
-        transform.position -= new Vector3(0, 0.25f, 0) * Time.deltaTime;
-    }
-
-    public void UpdateBatch()
-    {
-        GenerateBatchLayer(JsonConvert.SerializeObject(data));
-
-        data.Remove("layer" + start.ToString());
-        data.Add("layer" + (start + 17).ToString(), data["layer" + (start + 16).ToString()] + 1);
-        start++;
-
-        //for (int i = 0; i < data.Count; i++)
-        //{
-        //    data["layer" + (start + i).ToString()]--;
-        //}
-
-        print("料层");
-    }
-
-    public bool GenerateBatchLayer(string content)
-    {
-        for (int child = 0; child < transform.childCount;)
-        {
-            DestroyImmediate(transform.GetChild(child).gameObject);
-        }
-
         GameObject prefab = Resources.Load<GameObject>("Prefabs/layer");
+        GameObject obj = Instantiate(prefab, transform);
+        obj.transform.localPosition = from;
+        obj.transform.Find("Canvas").Find("number").GetComponent<Text>().text = number;
+        obj.name = number;
 
-        JToken items = JObject.Parse(content);
-        foreach (JProperty item in items)
+        while (obj.transform.position != target)
         {
-            string number = item.Name;
-            float height = float.Parse(item.Value.ToString());
-            GameObject obj = Instantiate(prefab, transform);
-            obj.transform.localPosition = new Vector3(2.5f, height, 0);
-            obj.transform.Find("Canvas").Find("number").GetComponent<Text>().text = number;
-            obj.name = number;
+            obj.transform.position = Vector3.MoveTowards(obj.transform.position, target, moveSpeed * Time.deltaTime);
+            yield return 0;
         }
-
-        return true;
+        DestroyImmediate(obj);
     }
+
+    public void Test()
+    {
+        StartCoroutine(GenerateLayer("layer" + count.ToString()));
+        count++;
+    }
+
+
+    //private Dictionary<string, float> data;
+    //private int start;
+    //private void Start()
+    //{
+    //    data = new Dictionary<string, float>();
+    //    for (int i = 0; i < 17; i++)
+    //    {
+    //        data.Add("layer" + i.ToString(), 23 + i);
+    //    }
+    //    start = 0;
+
+    //}
+
+    //private void Update()
+    //{
+    //    transform.position -= new Vector3(0, 0.25f, 0) * Time.deltaTime;
+    //}
+
+    //public void UpdateBatch()
+    //{
+    //    GenerateBatchLayer(JsonConvert.SerializeObject(data));
+
+    //    data.Remove("layer" + start.ToString());
+    //    data.Add("layer" + (start + 17).ToString(), data["layer" + (start + 16).ToString()] + 1);
+    //    start++;
+
+    //    //for (int i = 0; i < data.Count; i++)
+    //    //{
+    //    //    data["layer" + (start + i).ToString()]--;
+    //    //}
+
+    //    print("料层");
+    //}
+
+    //public bool GenerateBatchLayer(string content)
+    //{
+    //    for (int child = 0; child < transform.childCount;)
+    //    {
+    //        DestroyImmediate(transform.GetChild(child).gameObject);
+    //    }
+
+    //    GameObject prefab = Resources.Load<GameObject>("Prefabs/layer");
+
+    //    JToken items = JObject.Parse(content);
+    //    foreach (JProperty item in items)
+    //    {
+    //        string number = item.Name;
+    //        float height = float.Parse(item.Value.ToString());
+    //        GameObject obj = Instantiate(prefab, transform);
+    //        obj.transform.localPosition = new Vector3(2.5f, height, 0);
+    //        obj.transform.Find("Canvas").Find("number").GetComponent<Text>().text = number;
+    //        obj.name = number;
+    //    }
+
+    //    return true;
+    //}
 }
