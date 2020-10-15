a13af6d344c02e28337025eab7a9efa4a32d5144
fashionguy <3301225509@qq.com>
2020-10-15 22:49:04 +0800

    add headloadmanager

---
+++ Assets/HeatLoadManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatLoadManager : MonoSingleton<HeatLoadManager>
+{
+    
+
+    public bool HeatLoadUpdater(string content)
+    {
+        print(content);
+        print("heat");
+        return true;
+    }
+}
