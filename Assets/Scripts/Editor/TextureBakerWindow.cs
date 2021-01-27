811c269401ab0a9baf9fb4f54137267d4ac37b36
SaberStereo <stereo.bingo@gmail.com>
2021-01-27 12:17:46 +0800

    texture baker

---
+++ Assets/Scripts/Editor/TextureBakerWindow.cs
@@ -0,0 +1,109 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class TextureBakerWindow : EditorWindow
+{
+    GameObject objectToBake;
+
+    [MenuItem("Tools/Baking Texture")]
+    public static void Init()
+    {
+        TextureBakerWindow window = GetWindow(typeof(TextureBakerWindow)) as TextureBakerWindow;
+        window.minSize = new Vector2(400, 200);
+        window.Show();
+    }
+
+    private void OnGUI()
+    {
+        string bakeInfo = "";
+
+        GUIStyle titleStyle = new GUIStyle(GUI.skin.label)
+        {
+            alignment = TextAnchor.MiddleCenter,
+            fontStyle = FontStyle.Bold
+        };
+        titleStyle.fontSize = 16;
+        GUIStyle errorStyle = new GUIStyle()
+        {
+            alignment = TextAnchor.MiddleCenter,
+            fontStyle = FontStyle.Bold,
+            wordWrap = true
+        };
+
+        EditorGUILayout.LabelField("Bake Texture", titleStyle, GUILayout.ExpandWidth(true));
+
+        EditorGUILayout.BeginHorizontal();
+        objectToBake = EditorGUILayout.ObjectField("Object to Bake:", objectToBake, typeof(GameObject), true) as GameObject;
+        EditorGUILayout.EndHorizontal();
+
+        EditorGUILayout.BeginHorizontal();
+        int resolution = EditorGUILayout.IntField("Resolution", 1024);
+        EditorGUILayout.EndHorizontal();
+
+        if (objectToBake != null)
+        {
+            // Choose which submesh to bake or all mesh
+            Mesh selectedMesh = objectToBake.GetComponent<MeshFilter>().sharedMesh;
+            int subMeshCount = selectedMesh.subMeshCount;
+            int[] value = new int[subMeshCount + 1];
+            string[] info = new string[subMeshCount + 1];
+            for (int i = 0; i < subMeshCount; i++)
+            {
+                info[i] = "Submesh index <" + i.ToString() + ">";
+                value[i] = i;
+            }
+            value[subMeshCount] = subMeshCount;
+            info[subMeshCount] = "Full mesh";
+            // Only display this selection when submesh count > 1;
+            EditorGUILayout.BeginHorizontal();
+            EditorGUILayout.LabelField("SubMesh to Bake:");
+            int subMeshIndex = EditorGUILayout.IntPopup(0, info, value);
+            EditorGUILayout.EndHorizontal();
+
+            if (GUILayout.Button("Bake"))
+            {
+                if (subMeshIndex == subMeshCount) // draw full mesh
+                {
+                    Bake(selectedMesh, subMeshCount, resolution, true);
+                }
+                else
+                {
+                    Bake(selectedMesh, subMeshIndex, resolution);
+                }
+            }
+        }
+        else
+        {
+            bakeInfo = "Pls select mesh before baking.\n";
+            errorStyle.normal.textColor = Color.yellow;
+        }
+
+        // show info
+        GUI.Box(new Rect(0, position.height - 50, position.width - 20, 50), bakeInfo, errorStyle);
+    }
+
+    private void Bake(Mesh mesh, int subMesh, int res, bool fullMesh = false)
+    {
+        EditorApplication.ExecuteMenuItem("Edit/Play");
+
+        GameObject obj = new GameObject("TextureBaker");
+        TextureBaker baker = obj.AddComponent<TextureBaker>();
+
+        if (baker.LoadMaterials())
+        {
+            baker.resolution = res;
+            baker.mesh = mesh;
+            baker.subMeshIndex = subMesh;
+            baker.fullMesh = fullMesh;
+            baker.Bake();
+        }
+
+        Object.DestroyImmediate(obj);
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit();
+#endif
+    }
+}
