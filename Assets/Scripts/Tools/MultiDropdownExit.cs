234b3c3a9dab6ef81185e54333e945087062aa42
SaberStereo <stereo.bingo@gmail.com>
2021-04-04 15:02:51 +0800

    fixed auto rotate direction, and auto hide layer dropdown

---
+++ Assets/Scripts/Tools/MultiDropdownExit.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using Michsky.UI.ModernUIPack;
+
+public class MultiDropdownExit : MonoBehaviour, IPointerExitHandler
+{
+    public DropdownMultiSelect dropdown;
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        if (dropdown.outOnPointerExit == true)
+        {
+            if (dropdown.isOn == true)
+            {
+                dropdown.Animate();
+                dropdown.isOn = false;
+            }
+        }
+    }
+}
