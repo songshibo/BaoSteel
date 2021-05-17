a380ef8f728ff2b3ce715b046291d5a55022fef4
fashionguy <3301225509@qq.com>
2021-05-17 17:37:27 +0800

    Batch layer

--- Assets/Scripts/Updaters/BatchLayerUpdater.cs
+++ Assets/Scripts/Updaters/BatchLayerUpdater.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections;
+using TMPro;
 using UnityEngine;
 using UnityEngine.Formats.Alembic.Importer;
 
@@ -28,11 +29,20 @@ public class BatchLayerUpdater : MonoSingleton<BatchLayerUpdater>
     IEnumerator GenerateLayer(string number)
     {
         GameObject obj = Instantiate(prefab, transform);
+        Transform c = obj.transform.Find("Canvas");
+        c.Find("name").GetComponent<TMP_Text>().text = number;
+        float scale = obj.transform.Find("MateralLayer").localScale.x;  // 料层动画有缩放，计算顶点中心时，需要乘缩放值
+        MeshFilter mf = obj.transform.Find("MateralLayer").Find("material_layer").gameObject.GetComponent<MeshFilter>();
+
         AlembicStreamPlayer al = obj.GetComponent<AlembicStreamPlayer>();
         obj.name = number;
         ChangeLayer(obj.transform);
         for (float time=startTime; time < endTime * 5;)
         {
+            mf.sharedMesh.RecalculateBounds();
+
+            c.position = new Vector3(0, -mf.sharedMesh.bounds.center.z * scale, 0);
+            //print(mf.sharedMesh.bounds.center.x + "  " + mf.sharedMesh.bounds.center.y + "  " + mf.sharedMesh.bounds.center.z);
             // 设置obj的时间
             al.CurrentTime = time / 5;
             time += Time.fixedDeltaTime;
@@ -74,25 +84,25 @@ public class BatchLayerUpdater : MonoSingleton<BatchLayerUpdater>
     internal void Rotate(float angle)
     {
         transform.rotation = Quaternion.Euler(0, angle, 0);
-        transform.localPosition = new Vector3(0, 0, 0);
-        if (angle == 0)
-        {
-            transform.localPosition = new Vector3(0, 0, 0.01f);
-        }
-        else if (angle == 90)
-        {
-            transform.localPosition = new Vector3(0.01f, 0, 0);
-        }
-        else if (angle == 180)
-        {
-            transform.localPosition = new Vector3(0, 0, -0.01f);
-        }else if(angle == 270)
-        {
-            transform.localPosition = new Vector3(-0.01f, 0, 0);
-        }
-        else
-        {
-            Debug.LogError("这个是什么角度？？？");
-        }
+        //transform.localPosition = new Vector3(0, 0, 0);
+        //if (angle == 0)
+        //{
+        //    transform.localPosition = new Vector3(0, 0, 0.01f);
+        //}
+        //else if (angle == 90)
+        //{
+        //    transform.localPosition = new Vector3(0.01f, 0, 0);
+        //}
+        //else if (angle == 180)
+        //{
+        //    transform.localPosition = new Vector3(0, 0, -0.01f);
+        //}else if(angle == 270)
+        //{
+        //    transform.localPosition = new Vector3(-0.01f, 0, 0);
+        //}
+        //else
+        //{
+        //    Debug.LogError("这个是什么角度？？？");
+        //}
     }
 }
