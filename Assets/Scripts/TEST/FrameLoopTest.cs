3432c8d6e5a4c7081a9614759efbf0b30b08d831
SaberStereo <stereo.bingo@gmail.com>
2021-05-18 14:06:09 +0800

    add frame fbx/material/usage example scripts

---
+++ Assets/Scripts/TEST/FrameLoopTest.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameLoopTest : MonoBehaviour
+{
+    Mesh[] frames;
+    MeshFilter filter;
+    float time = 0;
+    [SerializeField]
+    int index = 0;
+    void Start()
+    {
+        List<Mesh> meshes = new List<Mesh>();
+        for (int i = 0; i <= 219; i++)
+        {
+            GameObject frame_mesh = Resources.Load<GameObject>("MaterialLayerAnimation/frame" + i.ToString());
+            meshes.Add(frame_mesh.GetComponent<MeshFilter>().sharedMesh);
+        }
+        frames = meshes.ToArray();
+        meshes.Clear();
+
+        filter = GetComponent<MeshFilter>();
+        filter.mesh = frames[index];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (time > 0.033f)
+        {
+            index++;
+            if (index > 219)
+            {
+                index = 0;
+            }
+            filter.mesh = frames[index];
+            time = 0;
+        }
+        time += Time.deltaTime;
+    }
+}
