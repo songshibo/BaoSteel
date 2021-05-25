cd390dc2815d7ec1d1b6a41331de1e7f78449b35
fashionguy <3301225509@qq.com>
2021-05-25 23:11:03 +0800

    success to get raycast

--- Assets/Scripts/Updaters/ResidualUpdater.cs
+++ Assets/Scripts/Updaters/ResidualUpdater.cs
@@ -28,6 +28,10 @@ public class ResidualUpdater : MonoSingleton<ResidualUpdater>
     public Texture2D residualThicknessTex;
     private Texture2D gradientTex;
 
+    private Mesh[] meshes;
+    private MeshFilter filter;
+    private MeshCollider mc;
+
     public void Initialize()
     {
         gradientTex = gradient.GetTexture(512, 1);
@@ -42,6 +46,51 @@ public class ResidualUpdater : MonoSingleton<ResidualUpdater>
             residualMaterial.SetFloat("_MinHeight", Util.BOTTOM_H);
             residualMaterial.SetTexture("_Gradient", gradientTex);
             UpdateKeyword();
+        }
+
+        meshes = new Mesh[5];
+        meshes[0] = Resources.Load<GameObject>("hearth-inside/inside-0").GetComponent<MeshFilter>().sharedMesh;
+        meshes[1] = Resources.Load<GameObject>("hearth-inside/inside-90").GetComponent<MeshFilter>().sharedMesh;
+        meshes[2] = Resources.Load<GameObject>("hearth-inside/inside-180").GetComponent<MeshFilter>().sharedMesh;
+        meshes[3] = Resources.Load<GameObject>("hearth-inside/inside-270").GetComponent<MeshFilter>().sharedMesh;
+        meshes[4] = Resources.Load<GameObject>("hearth-inside/inside-full").GetComponent<MeshFilter>().sharedMesh;
+
+        transform.gameObject.layer = 9;
+
+        if (!TryGetComponent(out filter))
+        {
+            filter = gameObject.AddComponent<MeshFilter>();
+        }
+        filter.sharedMesh = meshes[0];
+
+        if (!TryGetComponent(out mc))
+        {
+            mc = gameObject.AddComponent<MeshCollider>();
+        }
+        mc.sharedMesh = meshes[0];
+
+        MeshRenderer meshRenderer = gameObject.AddComponent<MeshRenderer>();
+        meshRenderer.sharedMaterial = Resources.Load<Material>("ClippingMaterials/inside_no_tess");
+        meshRenderer.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.Off;
+        meshRenderer.receiveShadows = false;
+    }
+
+    public void ClickAndShowResidualDetail(RaycastHit[] hitArr)
+    {
+        foreach (RaycastHit hit in hitArr)
+        {
+            if (hit.transform.name.Equals(transform.name))
+            {
+                if(hit.normal.y == 1)
+                {
+                    InvertSamplingFromRayCast(hit.point, true);
+                }
+                else
+                {
+                    InvertSamplingFromRayCast(hit.point, false);
+                }
+                break;
+            }
         }
     }
 
